4b13b2dea745f3381087030cf10772da13ea1d00
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-24 09:41:01 -0500

    Adding all files

---
+++ index.js
@@ -0,0 +1,11 @@
+/* global Vue */
+
+var app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      message: "Hello from JavaScript!",
+      name: "My name is Kiel",
+    };
+  },
+});
