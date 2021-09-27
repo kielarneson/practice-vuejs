a56a2b058c5ee78bc934b1cba7ce2497662f248a
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-27 10:15:57 -0500

    Added methods to send GET and POST web requests and html to display their web responses

--- todos.js
+++ todos.js
@@ -1,15 +1,32 @@
-/* global Vue */
+/* global Vue, axios */
 
 var app = new Vue({
   el: "#app",
   data: function () {
     return {
       message: "Hello from JavaScript!",
+      todos: [],
+      postTodo: [],
     };
   },
   methods: {
     loadTodos: function () {
-      console.log("hello");
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      });
+    },
+    postTodos: function () {
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          userId: 23,
+          title: "Meet the Flintstones",
+          completed: true,
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.postTodo = response.data;
+        });
     },
   },
 });
